b18680fb0ca2a3e82553c14e51e28c5108badcf5
Gaelle CLOCHARD <gaelle.clocha@gmail.com>
2023-01-29 22:04:38 +0100

    Lecture des dates au bon format

---
+++ src/InputReader.ts
@@ -0,0 +1,21 @@
+import promptSync from 'prompt-sync';
+import { isMatch, parseISO, format } from 'date-fns'
+
+export const dateFormat: string = 'dd-MM-yyyy';
+const prompt = promptSync();
+
+class InputReader {
+  
+  static readDate(questionText: string) {
+    let date = prompt(questionText);
+    if (date !== null && isMatch(date, dateFormat)) {
+        return format(parseISO(date), dateFormat);
+    } else {
+      console.log(`${date} does not match format ${dateFormat}`);
+      return undefined;
+    }
+  }
+
+}
+
+export default InputReader;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Welcome");
+import InputReader, {dateFormat} from "./InputReader"
+
+
+function readInputs() {
+  let dateFrom = InputReader.readDate("Entrer une date de debut (DD-MM-YYYY): ");
+  
+  let dateTo = InputReader.readDate("Entrer une date de fin (DD-MM-YYYY): ");
+}
+
+readInputs();
